b641a5b57650547e0cd6212bb283a60215920ad7
artem <mai888sutalkuar@gmail.com>
2025-02-13 21:07:44 +0000

    123

--- cykavi_tasks/task2.cpp
+++ cykavi_tasks/task2.cpp
@@ -1,33 +1,22 @@
 #include <iostream>
 
 int main() {
-
     int n;
     std::cin >> n;
+    std::cout << std::endl;
+    int pascal[n][n];
 
-    int** pasc_triangle  = new int*[n];
-    for (int i = 0; i < n; ++i) {
-        pasc_triangle[i] = new int[i + 1];
-    }
-
-    for (int i = 0; i < n; ++i) {
-        pasc_triangle[i][0] = pasc_triangle[i][i] = 1;
-        for (int j = 1; j < i; ++j) {
-            pasc_triangle[i][j] = pasc_triangle[i - 1][j - 1] + pasc_triangle[i - 1][j];
-        }
-    }
-
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j <= i; ++j) {
-            std::cout << pasc_triangle[i][j] << " ";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            if (j == 0 || j == i) {
+                pascal[i][j] = 1;
+            } else {
+                pascal[i][j] = pascal[i - 1][j - 1] + pascal[i - 1][j];
+            }
+            std::cout << pascal[i][j] << " ";
         }
         std::cout << std::endl;
     }
 
-    for (int i = 0; i < n; ++i) {
-        delete[] pasc_triangle[i];
-    }
-    delete[] pasc_triangle; 
-
-    return 0;
+return 0;
 }
